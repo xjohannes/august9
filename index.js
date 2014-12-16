65e3c208f5528c78e4163a56d67fefa8b647629b
Johannes Akse <johannes.akse@gmail.com>
2014-12-16 11:50:05 +0100

    config var test

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(coolFaces());
+	var result = '';
+	var times = process.env.TIMES || 5;
+	for( var i = 0; i < times; i++) {
+		result += coolFaces();
+	}
+  response.send(result;
 });
 
 app.listen(app.get('port'), function() {

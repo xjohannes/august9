037b6c42141cbc427cc9d0d02b85eff99f2a510e
Johannes Akse <johannes.akse@gmail.com>
2014-12-16 14:59:07 +0100

    psql

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.get('/', function(request, response) {
 });
 
 app.get('/db', function( request, response ) {
-	pg.connect(process.env.DATABASEURL, function( err, client, done ) {
-		/*
+	pg.connect(process.env.DATABASE_URL, function( err, client, done ) {
+		
 		client.query('SELECT * FROM test_table', function( err, result) {
 			done();
 			if(err) {
@@ -28,7 +28,7 @@ app.get('/db', function( request, response ) {
 				response.send(result.rows);
 			}
 		});
-*/
+
 	});
 });
 

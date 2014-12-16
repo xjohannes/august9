a8f6f1210a09abe965af74e3a82d08982a701354
Johannes Akse <johannes.akse@gmail.com>
2014-12-16 11:16:30 +0100

    Demo first commig

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var coolFaces = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(coolFaces());
 });
 
 app.listen(app.get('port'), function() {

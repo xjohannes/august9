6bcc405d9445b481e89583b0975e4d8137af1180
Johannes Akse <johannes.akse@gmail.com>
2014-12-19 15:18:04 +0100

    rearr filestruct

---
+++ public/mocks/august9.js
@@ -0,0 +1,11 @@
+//* August 9 main js file
+/*
+/*
+*/
+(function () {
+'use strict';
+var august9 = Angular.module('August9', ['controllers', 'services', 'mockServer', 
+	'ui.router', 'ngResource']);
+
+// End outer wrapper function
+}()); 
--- public/mocks/serverMock.js
+++ public/mocks/serverMock.js
@@ -1,1 +1,8 @@
-s
+(function () {
+'use strict';
+var serverMock = Angular.module('ServerMock', function () {
+
+});
+
+// End outer wrapper function
+}()); 

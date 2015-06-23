afcadbf37782014380fd4cefa1061a5b42bffb0a
Johannes Akse <johannes.akse@gmail.com>
2015-06-23 16:33:21 +0200

    testing to move index.js to public

--- public/scripts/august9.js
+++ public/scripts/august9.js
@@ -9,38 +9,11 @@ var august9 = angular.module('august9', ['MockServer',
 
 
 august9.config( function( $provide, $stateProvider, $urlRouterProvider ) {
-	$urlRouterProvider.otherwise("/");
-$stateProvider
-		 // HOME STATES AND NESTED VIEWS ========================================
-    .state('august9', {
-        url: '/',
-        templateUrl: './partials/home.html'
-    })
-  
-		.state('genre', {
-			url: "/:genre/:track",
-			views: {
-				"": {templateUrl: "./partials/genre.html",
-							controller: function ($scope , $stateParams, singer, electro, $state ) {
-								$scope.tracks = ($stateParams.genre === 'singer' ? singer : electro);
-								$scope.selectedGenre = $stateParams.genre;
-								$scope.selectedTrack = "../media/music/" + $scope.selectedGenre 
-								+ '/' + $stateParams.track;
-							}},
-				"playerView": {
-					templateUrl:'./partials/playerView.html',
-					controller: function ($scope, $stateParams) {
-						$scope.selectedTrack = "../media/music/" + $stateParams.genre 
-								+ '/' + $stateParams.track + '.mp3';
-						console.log("genre State: " + $scope.selectedTrack);
-					} 
-				},
-				"trackStats": {templateUrl: "./partials/trackStats.html"},
-				"trackComments": {templateUrl: "./partials/trackComments.html"}
-			}
-		});
+	//$urlRouterProvider.otherwise("/");
+
+	
+		
 //This works
-/*
 	$stateProvider
 		 // HOME STATES AND NESTED VIEWS ========================================
     .state('home/', {
@@ -70,9 +43,13 @@ $stateProvider
 				"trackComments": {templateUrl: "./partials/trackComments.html"}
 			}
 		})
+		.state('upload', {
+			url: "/upload",
+			templateUrl: "./partials/uploadTrack.html"
+		})
 		;
-		*/
-});
+	});	
+
 
 // End outer wrapper function
-}()); 
+}());
--- public/scripts/services.js
+++ public/scripts/services.js
@@ -6,12 +6,15 @@
 var august9Services = angular.module('august9Services', ['ngResource']);
 
 // Using $resource
-august9Services.factory('singer', ['$resource',
+august9Services.factory('Singer', ['$resource',
   function($resource){
 
     return {'track1': 'I WILL find you (vidar)',
-            'track2': 'twilightVidarMusikmakarna'};
+            'track2': 'twilightVidarMusikmakarna',
+            'track3': '7aa4d0a787ca92e67d97e70b47e238ce'};
   }]);
+
+
 /*
 // Using $http
 august9Services.factory('Genome', ['$http',
@@ -22,10 +25,17 @@ august9Services.factory('Genome', ['$http',
   }]);
 */
 
-august9Services.factory('electro', ['$resource',
+august9Services.factory('Electro', ['$resource',
   function($resource){
     return {'track1': 'kuldeSjokk',
             'track2': 'hotel_boryfsen'};
+  }]);
+
+august9Services.factory('singer', ['$resource',
+  function($resource){
+
+    return {'track1': 'I WILL find you (vidar)',
+            'track2': 'twilightVidarMusikmakarna'};
   }]);
 
 // End

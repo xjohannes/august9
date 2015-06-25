5519f8e4556ca1d855f897f1e9ca661c3bc5a5f2
Johannes Akse <johannes.akse@gmail.com>
2015-06-25 19:51:14 +0200

    db2

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/', function(request, response) {
 });
 
 app.get('/db', function(request, response) {
-	console.log(process.env.DATABASE_URL);
+	var x = process.env.DATABASE_URL;
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 		client.query('SELECT * FROM test_table2', function(err, result) {
 			done();
@@ -34,7 +34,7 @@ app.get('/db', function(request, response) {
 				console.error(err);
 				response.send("Error " + err);
 			} else {
-				response.send(result.rows);
+				response.send(x);
 			}
 		});
 	});

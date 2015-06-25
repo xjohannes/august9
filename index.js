282bf6d502124c558d8259a71f7590361bb6608f
Johannes Akse <johannes.akse@gmail.com>
2015-06-25 11:15:45 +0200

    cool faces demo

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
+var cool = require('cool-ascii-faces');
 //var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+
+
+
+//app.use(express.static(__dirname + '/public'));
 
 // routes
+app.get('/', function(request, response) {
+  response.send(cool());
+});
+
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });

238295270a31d930dec979d4cbc33434e09da921
Johannes Akse <johannes.akse@gmail.com>
2015-06-25 19:58:26 +0200

    db2

--- index.js
+++ index.js
@@ -22,20 +22,18 @@ app.get('/', function(request, response) {
 	for( var i = 0; i < times; i++) {
 		result += coolFaces();
 	}
-  response.send(process.env.DATABASE_URL);
-
+  response.send(result);
 });
 
 app.get('/db', function(request, response) {
-	var x = process.env.DATABASE_URL;
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM test_table2', function(err, result) {
+		client.query('SELECT * FROM test_table', function(err, result) {
 			done();
 			if(err) {
 				console.error(err);
 				response.send("Error " + err);
 			} else {
-				response.send(x);
+				response.send(result.rows);
 			}
 		});
 	});

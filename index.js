824ca5a544e0d1b99224a4f370e2da8575247fdb
Johannes Akse <johannes.akse@gmail.com>
2015-06-25 20:35:35 +0200

    added DATABASE_URL to localhost

--- index.js
+++ index.js
@@ -28,9 +28,8 @@ app.get('/', function(request, response) {
 });
 
 app.get('/db', function(request, response) {
-	
+	response.send("Hi there baby I'm Bobby Brown");
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		response.send("Hi there baby I'm Bobby Brown");
 		client.query('SELECT * FROM test_table', function(err, result) {
 			done();
 			if(err) {
